b21999f6215f46a4f866144540628b98d30eef84
Clifton Craig <clifton.craig@gmail.com>
2020-07-19 23:39:03 -0700

    Add file system saving.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const https = require('https');
+const fs = require('fs');
 
 const get = (url, headers) => new Promise((resolve, reject) => {
     const callback = (res) => {
@@ -8,17 +9,30 @@ const get = (url, headers) => new Promise((resolve, reject) => {
     else https.get(url, callback);
 });
 
-url = 'https://api.github.com/users/cliff76';
+const writeFile = (fd, data) => {
+    return new Promise((resolve, reject) => {
+        fs.write(fd, data, (err, numBytes) => { err ? reject(err) : resolve(numBytes) });
+    });
+};
+
+const usingFile = (f) => {
+    const resolveFile = () => new Promise((resolve, reject) => {
+        fs.open(f, 'w+',(err, fd) => err ? reject(err) : resolve(fd) );
+    });
+    return {
+        resolveFile,
+        write: (data) => { return resolveFile().then(fd => writeFile(fd, data))}
+    };
+};
+
 const headers = {
     'Accept': 'application/vnd.github.v3+json',
     'User-Agent': 'nodeJS'
 };
-get(url,headers).then(({data, statusCode}) => {
+get('https://api.github.com/users/cliff76',headers).then(({data, statusCode}) => {
     console.log('Response code: ' + statusCode);
     console.log(data.toString());
+    return usingFile('./githubResponse.txt').write(data);
 }).catch(e => {
-    console.error('HTTPS Error:', data)
+    console.error('HTTPS Error:', e)
 });
-
-exports.https = https;
-exports.get = get;
